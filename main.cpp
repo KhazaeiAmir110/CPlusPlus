6e033efcc723ea175158890a6dc20be604218acd
test_python <khazaei.amir110@gmail.com>
2023-10-22 10:14:38 +0330

    cout

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+int main(){
+    std::cout <<"Hello C++";
+    cout << "using namespace std";
 }
