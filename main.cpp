78e8c9153c31b2147fbc1b531cbe6a200481903d
test_python <khazaei.amir110@gmail.com>
2023-10-23 19:29:58 +0330

    Practice IF,els

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Please enter your age: " << endl;
-    int age;
-    cin >> age;
-    if (age >= 18){
-        if (age < 25){
-            cout << "You are a young." << endl;
-        } else{
-            cout << "You are a adult." << endl;
-        }
-    } else {
-        cout << "Sorry, you are not old enough."
-        << endl;
+    cout << "Please enter your Salary: ";
+    int salary,income=0,tax=0;
+    cin >> salary;
+    if (salary <= 1000){
+        income = salary* (10.0/100);
+        tax = salary-income;
+        cout << "Your income is: " << income << endl;
+        cout << "Your tax is: " << tax << endl;
+    } else if (salary > 1000 && salary <= 2000){
+        income = salary* (15.0/100);
+        tax = salary-income;
+        cout << "Your income is: " << income << endl;
+        cout << "Your tax is: " << tax << endl;
+    } else if (salary > 2000 && salary < 5000){
+        income = salary* (20.0/100);
+        tax = salary-income;
+        cout << "Your income is: " << income << endl;
+        cout << "Your tax is: " << tax << endl;
+    } else if (salary == 5000){
+        income = salary* (25.0/100);
+        tax = salary-income;
+        cout << "Your income is: " << income << endl;
+        cout << "Your tax is: " << tax << endl;
     }
 }
