0d960c5e9e944c1b6561516d3512f54437d44189
test_python <khazaei.amir110@gmail.com>
2023-10-25 09:05:42 +0330

    Array List

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-//    switch - case
-    int code;
-    cout << "Please Enter city code : ";
-    cin >> code;
-    switch (code) {
-        case 21:
-            cout << "Tehran";
-            break;
-        case 31:
-            cout << "Isfahan";
-            break;
-        case 41:
-            cout << "Tabriz";
-            break;
-        default:
-            cout << "wrong code!!";
-    }
+    int myArray[10] = {1,3,5,7,9,11,13,15,17,19};
+//    index array
+    cout <<  "myArray[0] = " << myArray[0] << endl;
+    cout <<  "myArray[5] = " << myArray[5] << endl;
+//    chang array
+    myArray[5] = 100;
+    cout << "myArray[5] , change = " << myArray[5] << endl;
 
+//    for
+    int total=0;
+    for (int i = 0; i < 10; i++){
+        cout << "myArray[" << i << "] = " << myArray[i] << endl;
+        total+=myArray[i];
+    }cout << "Total : " << total << endl;
+    return 0;
 }
