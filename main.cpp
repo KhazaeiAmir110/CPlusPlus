4d11a7674815b5460273d0d32d7b954526f15105
test_python <khazaei.amir110@gmail.com>
2023-10-25 21:36:31 +0330

    start work with function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
+int mySum(int a, int b){
+    cout << "sum a , b : " << a + b << endl;
+}
+
+void sayHello(){
+    cout << "Hello World!" << endl;
+}
+
 int main(){
-    int x = 5;//stack
-    float y = 10.7;//
-
-//    heap
-    int * point = new int;
-    *point = 10;
-    cout << *point << endl;
-
-    delete point;
-    cout << *point << endl;
+    mySum(1, 2);
+    sayHello();
 }
